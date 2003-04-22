3774eef978823beb2cdc8c945b3290d7a3a31f26
Seth Vidal <skvidal@linux.duke.edu>
2003-04-22 02:06:29 +0000

    fix for grouplist w/o groups

--- pkgaction.py
+++ pkgaction.py
@@ -311,9 +311,9 @@ def listgroups(userlist):
         if userlist[0] == "hidden":
             groups = GroupInfo.grouplist
             userlist.pop(0)
-        else:
-            groups = GroupInfo.visible_groups
-
+    else:
+        groups = GroupInfo.visible_groups
+    
     if len(userlist) == 0:
         userlist = ['_all_']
 
